03a14182e9931221f8b899f2e81771054ea5357d
willjw3 <will.j.ward3@gmail.com>
2020-10-24 13:14:28 +0900

    Removed twitter link

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,7 @@ const Footer = ({content, siteTitle}) => {
         <div className="footer">
           <hr/>
           <div className={`footer-${content}`}>
-            <a href="https://twitter.com/"><img className={`footer-${content}-twitter`} src={Twitter} aria-label="Twitter link" /></a>
+            
             <p>© {new Date().getFullYear()}, {siteTitle}</p>
           </div> 
         </div>
